88599388b24956ef1e0f3a8e0fcd579cff26ae9a
Aaron Gerig <a.gerig@w-vision.ch>
2019-05-14 17:47:35 +0200

    finished chapter 2 and 3

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '@/views/Home'
+import ThreadShow from '@/views/ThreadShow'
+import NotFound from '@/views/NotFound'
 
 Vue.use(Router)
 
@@ -10,16 +12,19 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/thread/:id',
+      name: 'ThreadShow',
+      component: ThreadShow,
+      props: true
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="col-full">
+    <h1>Welcome to the Forum</h1>
+    <ThreadList :threads="threads" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import sourceData from '@/data'
+import ThreadList from '@/components/ThreadList'
+console.log(sourceData)
 
 export default {
-  name: 'home',
+  name: 'Home',
+
   components: {
-    HelloWorld
+    ThreadList
+  },
+
+  data () {
+    return {
+      threads: Object.values(sourceData.threads),
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
   }
 }
 </script>
