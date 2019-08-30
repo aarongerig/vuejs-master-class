2392511762552adb8b13d8f35d1883d956a8740c
Aaron Gerig <a.gerig@w-vision.ch>
2019-08-30 17:24:27 +0200

    started working on chapter 16

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import firebase from 'firebase/app'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import router from './router'
 import store from '@/store'
 import AppDate from '@/components/AppDate'
 
+Vue.use(Vuelidate)
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
--- src/views/UserRegistration.vue
+++ src/views/UserRegistration.vue
@@ -16,27 +16,60 @@
             v-model="form.name"
             type="text"
             class="form-input"
+            @blur="$v.form.name.$touch()"
           >
+          <template v-if="$v.form.name.$error">
+            <span
+              v-if="!$v.form.name.required"
+              class="form-error"
+            >This field is required</span>
+          </template>
         </div>
 
         <div class="form-group">
           <label for="username">Username</label>
           <input
             id="username"
-            v-model="form.username"
+            v-model.lazy="form.username"
             type="text"
             class="form-input"
+            @blur="$v.form.username.$touch()"
           >
+          <template v-if="$v.form.username.$error">
+            <span
+              v-if="!$v.form.username.required"
+              class="form-error"
+            >This field is required</span>
+            <span
+              v-else-if="!$v.form.username.unique"
+              class="form-error"
+            >Sorry! This username is taken</span>
+          </template>
         </div>
 
         <div class="form-group">
           <label for="email">Email</label>
           <input
             id="email"
-            v-model="form.email"
+            v-model.lazy="form.email"
             type="email"
             class="form-input"
+            @blur="$v.form.email.$touch()"
           >
+          <template v-if="$v.form.email.$error">
+            <span
+              v-if="!$v.form.email.required"
+              class="form-error"
+            >This field is required</span>
+            <span
+              v-else-if="!$v.form.email.email"
+              class="form-error"
+            >This is not a valid email address</span>
+            <span
+              v-else-if="!$v.form.email.unique"
+              class="form-error"
+            >Sorry! This email is taken</span>
+          </template>
         </div>
 
         <div class="form-group">
@@ -46,7 +79,18 @@
             v-model="form.password"
             type="password"
             class="form-input"
+            @blur="$v.form.password.$touch()"
           >
+          <template v-if="$v.form.password.$error">
+            <span
+              v-if="!$v.form.password.required"
+              class="form-error"
+            >This field is required</span>
+            <span
+              v-else-if="!$v.form.email.minLength"
+              class="form-error"
+            >The password must be at least 6 characters long</span>
+          </template>
         </div>
 
         <div class="form-group">
@@ -56,7 +100,14 @@
             v-model="form.avatar"
             type="text"
             class="form-input"
+            @blur="$v.form.avatar.$touch()"
           >
+          <template v-if="$v.form.avatar.$error">
+            <span
+              v-if="!$v.form.avatar.url"
+              class="form-error"
+            >This is not a valid url</span>
+          </template>
         </div>
 
         <div class="form-actions">
@@ -78,6 +129,16 @@
 </template>
 
 <script>
+import firebase from 'firebase/app'
+import 'firebase/database'
+import {
+  email,
+  helpers as vuelidateHelpers,
+  minLength,
+  required,
+  url
+} from 'vuelidate/lib/validators'
+
 export default {
   name: 'UserRegistration',
 
@@ -93,12 +154,53 @@ export default {
     }
   },
 
+  validations: {
+    form: {
+      name: { required },
+      username: {
+        required,
+        unique (value) {
+          if (!vuelidateHelpers.req(value)) {
+            return true
+          }
+
+          return new Promise((resolve) => {
+            firebase.database().ref('users').orderByChild('usernameLower').equalTo(value.toLowerCase())
+              .once('value', snapshot => resolve(!snapshot.exists()))
+          })
+        }
+      },
+      email: {
+        email,
+        required,
+        unique (value) {
+          if (!vuelidateHelpers.req(value)) {
+            return true
+          }
+
+          return new Promise((resolve) => {
+            firebase.database().ref('users').orderByChild('email').equalTo(value.toLowerCase())
+              .once('value', snapshot => resolve(!snapshot.exists()))
+          })
+        }
+      },
+      password: { minLength: minLength(6), required },
+      avatar: { url }
+    }
+  },
+
   created () {
     this.$emit('ready')
   },
 
   methods: {
     register () {
+      this.$v.form.$touch()
+
+      if (this.$v.form.$invalid) {
+        return
+      }
+
       this.$store.dispatch('auth/registerUserWithEmailAndPassword', this.form)
         .then(() => this.successRedirect())
     },
